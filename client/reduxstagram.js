8f91cf121aeb21efb3c551d8d2eb6a150bf1c876
Cihan <cihannisan@gmail.com>
2016-09-21 20:10:31 -0700

    Add routes for components

--- client/reduxstagram.js
+++ client/reduxstagram.js
@@ -1,1 +1,22 @@
-// let's go!
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { Router, Route, browserHistory, IndexRoute } from 'react-router';
+
+//Import Css
+import css from './styles/style.styl';
+
+//Import Components
+import Main from './components/Main';
+import PhotoGrid from './components/PhotoGrid';
+import Single from './components/Single';
+
+const routes = (
+  <Router history={browserHistory}>
+  	<Route path='/' component={Main}>
+  	  <IndexRoute component={PhotoGrid}></IndexRoute>
+      <Route path='/view/:postId' component={Single}></Route>
+  	</Route>
+  </Router>
+);
+
+ReactDOM.render(routes ,document.getElementById('root'));
